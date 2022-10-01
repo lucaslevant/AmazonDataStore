c98998062f140a0d6306da8aa3313fb9581b1bb2
lucaslevant <114788936+lucaslevant@users.noreply.github.com>
2022-10-01 01:33:03 -0700

    Update book.cpp

--- book.cpp
+++ book.cpp
@@ -4,22 +4,17 @@
 #include "product.h"
 #include "util.h"
 
-
-
 Book::Book(const std::string category, std::string name, double price, int qty, std::string isbn, std::string author):
 		Product(category, name, price, qty),
-    isbn_(isbn),
+    		isbn_(isbn),
 		author_(author)
-
 {
 
 }
-
 Book::~Book()
 {
 
 }
-
   std::set<std::string> Book::keywords() const{
 	std::set<std::string> c;
   std::string key = convToLower(name_) + " " + convToLower(author_);
@@ -28,8 +23,6 @@ Book::~Book()
 	c.insert(isbn_);
 	return c;
 }
-
-
   std::string Book::displayString() const{
 	std::string final;
 	std::stringstream stream;
@@ -49,7 +42,6 @@ Book::~Book()
 	return final;
 	
 }
-
  void Book::dump(std::ostream& os) const{
 	os << category_ << std::endl << name_ << std::endl << price_ << std::endl << qty_ << std::endl << isbn_ << std::endl << author_ << std::endl;
 }
