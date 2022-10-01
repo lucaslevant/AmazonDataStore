92d8adc18746d6cfc91bdd50da2e87ad9d3dd726
lucaslevant <114788936+lucaslevant@users.noreply.github.com>
2022-10-01 02:12:22 -0700

    Update user.h

--- user.h
+++ user.h
@@ -3,10 +3,6 @@
 #include <iostream>
 #include <string>
 
-/**
- * Implements User functionality and information storage
- *  You should not need to derive anything from User at this time
- */
 class User {
 public:
     User();
