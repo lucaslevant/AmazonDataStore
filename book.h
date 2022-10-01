bb9a1f98a3bfec61cd60401e5e78a327079053ff
lucaslevant <114788936+lucaslevant@users.noreply.github.com>
2022-10-01 02:20:27 -0700

    Update book.h

--- book.h
+++ book.h
@@ -6,13 +6,13 @@
 #include "product.h"
 
 class Book : public Product {
-	public:
-	Book(const std::string category, std::string name_, double price, int qty, std::string isbn, std::string author);
+public:
+    Book(const std::string category, std::string name_, double price, int qty, std::string isbn, std::string author);
     ~Book();
-		std::set<std::string> keywords() const override;
+    std::set<std::string> keywords() const override;
     std::string displayString() const override;
     void dump(std::ostream& os) const override;
-	protected:
-		std::string isbn_;
+protected:
+    std::string isbn_;
     std::string author_;
 };
