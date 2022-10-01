9e1a3c6e555340c22db291696652c7b5832db60b
lucaslevant <114788936+lucaslevant@users.noreply.github.com>
2022-10-01 02:00:54 -0700

    Update mydatastore.cpp

--- mydatastore.cpp
+++ mydatastore.cpp
@@ -9,7 +9,6 @@
 
 
 
-
 MyDataStore::MyDataStore()
 {
 
@@ -17,28 +16,20 @@ MyDataStore::MyDataStore()
 
 MyDataStore::~MyDataStore()
 {
-	//what to put here? de allocate amazon ?
 
 }
 
 void MyDataStore::addProduct(Product* thing){ //how do i get product name as string
 	prod.push_back(thing);
-  std::set<std::string> words = thing->keywords();
+  	std::set<std::string> words = thing->keywords();
 	std::set<std::string>::iterator itr;
-	// for(itr = words.begin(); itr !=  words.end(); itr++){
-	// 	cout << *itr << " ";
-	// }
-	// cout << endl;
-
 	for(itr = words.begin(); itr !=  words.end(); itr++){
-		if(prodCart.find(*itr) == prodCart.end()){ //is word in prodCart
-			// cout << *itr << " adding new pair" << endl;
+		if(prodCart.find(*itr) == prodCart.end()){ 
 			std::set<Product*> temp;
 			temp.insert(thing);
 			prodCart[*itr] = temp;
 		}
 		else if(prodCart.find(*itr) != prodCart.end()){
-			// cout << *itr << " inserting" << endl;
 			prodCart.find(*itr)->second.insert(thing);
 		}
 	}
@@ -57,28 +48,22 @@ std::vector<Product*> MyDataStore::search(std::vector<std::string>& words, int c
 	int count = 0;
 	int size = words.size();
 for(int i = 0; i < size; i++){
-		if(prodCart.find(words[i]) != prodCart.end()){ //if word is in prodCart
-		// cout << "product found with word " << words[i] << endl;
-			if(count == 0){ 
-				holder = prodCart[words[i]];
+		if(prodCart.find(words[i]) != prodCart.end()){ 
+			if(count == 0){
+			   holder = prodCart[words[i]];
 			}
 			if(check == 0){
 			  holder = setIntersection(holder, prodCart[words[i]]);
 			  count++;
 			}
 			else if (check == 1){
-				holder = setUnion(holder, prodCart[words[i]]);
-			  count++;
+		           holder = setUnion(holder, prodCart[words[i]]);
+			   count++;
 			}		
 		}
 	}
 
 	std::vector<Product*> final(holder.begin(), holder.end());
-	// cout << final.size() << endl;
-	// for(int i = 0; i < final.size(); i++){
-	// 	cout << "product found " << final[i]->getName() << endl;
-	// }
-	
 	return final;
 }
 
@@ -87,21 +72,17 @@ for(int i = 0; i < size; i++){
  void MyDataStore::dump(std::ostream& ofile){
 
  ofile << "<product>" << std::endl;
-	 //loop through product vector and call dump on each product
 	int size = prod.size();
 	for(int i = 0; i < size; i++){
 		prod[i]->dump(ofile);
 	}
-
 	ofile << "</product>" << std::endl;
 	ofile << "<users>" << std::endl;
-  std::map<std::string, User*>::iterator it;
+  	std::map<std::string, User*>::iterator it;
 	for(it = users.begin(); it != users.end(); it++){
 		it->second->dump(ofile);
 	}
-
 	ofile << "</users>" << std::endl;
-
  }
 
 void MyDataStore::addCart(std::string username, Product* product){ //if username exists, add a cart
@@ -111,14 +92,12 @@ void MyDataStore::addCart(std::string username, Product* product){ //if username
 	}
 	usersCart.find(username)->second.push_back(product);
 }
-
-
 void MyDataStore::viewCart(std::string username){
 	if(users.find(username) == users.end()){
 		std::cout << "Invalid username" << std::endl;
 		return;
 	}		  
-  std::map<std::string, std::vector<Product*>>::iterator itr = usersCart.find(username);
+  	std::map<std::string, std::vector<Product*>>::iterator itr = usersCart.find(username);
 	int size = itr->second.size();
 	for(int i = 0; i < size; i++){
 		std::cout << itr->second[i]->displayString() << std::endl;
@@ -130,13 +109,11 @@ void MyDataStore::buyCart(std::string username){
 			std::cout << "Invalid username" << std::endl;
 			return;
 		}
-	  std::vector<Product*> holder;
+	 	std::vector<Product*> holder;
 		double balance = users.find(username)->second->getBalance();
-	  std::map<std::string, std::vector<Product*>>::iterator itr = usersCart.find(username);
-		// vector<Product*> temp = itr->second;
+	  	std::map<std::string, std::vector<Product*>>::iterator itr = usersCart.find(username);
 		int size = itr->second.size();
 		for(int i = size-1; i >= 0; i--){
-			// cout << "hello" << endl;
 			double price = itr->second[i]->getPrice();
 			if(itr->second[i]->getQty() > 0 && balance >= price){ //if user can afford and it is in qty
 				std::cout << "Thank you for purchasing " << itr->second[i]->getName() << std::endl;
