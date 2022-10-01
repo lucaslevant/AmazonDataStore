2f0acf323e6f5cb2c61fd4817b44a38eb46bec5d
lucaslevant <114788936+lucaslevant@users.noreply.github.com>
2022-10-01 01:40:32 -0700

    Update and rename clothing (3).h to clothing.h

--- clothing.h
+++ clothing.h
@@ -7,15 +7,13 @@
 
 
 class Clothing : public Product {
-	public:
-	Clothing(const std::string category, std::string name, double price, int qty, std::string size, std::string brand);
+public:
+     Clothing(const std::string category, std::string name, double price, int qty, std::string size, std::string brand);
     ~Clothing();
-		 std::set<std::string> keywords() const override;
+     std::set<std::string> keywords() const override;
      std::string displayString() const override;
      void dump(std::ostream& os) const override;
-	protected:
-
-		std::string size_;
+protected:
+    std::string size_;
     std::string brand_;
-
 };
